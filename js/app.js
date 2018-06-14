c08f59c32fe3091d03429cf5fd4d650522aaa9d6
Admin <GA-Admin@Admins-MacBook-Pro-2.local>
2018-06-14 09:10:49 -0500

    question 7 is kind of done but not all the way not because of problems but because I need to turn it in

--- js/app.js
+++ js/app.js
@@ -38,10 +38,59 @@
 // console.log(matt)
 // kristyn[3] = "Brooklyn"
 // console.log(kristyn)
-const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
-	for (let i = 0; i < ninjaTurtles.length; i++){
-		console.log(ninjaTurtles[i].toUpperCase());
-	}
+// const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+// 	for (let i = 0; i < ninjaTurtles.length; i++){
+// 		console.log(ninjaTurtles[i].toUpperCase());
+// 	}
+
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+const shoe = kristynsCloset.shift()
+thomsCloset[2].push(shoe)
+console.log(thomsCloset[2]) 
+const kristynOutside = [kristynsCloset[0], kristynsCloset[2], kristynsCloset[3]]
+const kristynOffice = [kristynsCloset[2], kristynsCloset[3], kristynsCloset[4]]
+console.log(`Kristyn is wearing ` + )
+
+
+
+
+
+
+
+
+
+
+
 
 
 
