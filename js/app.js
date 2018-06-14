8afb1cea52c9cba2075d268337272c689733f182
Admin <GA-Admin@Admins-MacBook-Pro-2.local>
2018-06-13 19:44:34 -0500

    question 5 is done that last one was 4

--- js/app.js
+++ js/app.js
@@ -15,14 +15,26 @@
 
 // 	}
 // 
-for (let i = 1; i <= 100; i++) {
-	if (i % 3 === 0 && i % 5 === 0){
-		console.log("FizzBuzz");
-	}else if (i % 5 === 0){
-		console.log("Buzz");
-	}else if (i % 3 === 0){
-		console.log("Fizz");
-	}else {
-		console.log(i);
-	}
-}
+// for (let i = 1; i <= 100; i++) {
+// 	if (i % 3 === 0 && i % 5 === 0){
+// 		console.log("FizzBuzz");
+// 	}else if (i % 5 === 0){
+// 		console.log("Buzz");
+// 	}else if (i % 3 === 0){
+// 		console.log("Fizz");
+// 	}else {
+// 		console.log(i);
+// 	}
+// }
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+thom[0] = "Gameboy"
+console.log(thom)
+karolin[1] = 16++
+console.log(karolin)
+matt[3] = "Gotham City"
+console.log(matt)
+kristyn[3] = "Brooklyn"
+console.log(kristyn)
