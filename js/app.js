ede02f82a55a2fcd146d1572a5de132fb3ff29b8
Admin <GA-Admin@Admins-MacBook-Pro-2.local>
2018-06-14 09:35:22 -0500

    homeworks

--- js/app.js
+++ js/app.js
@@ -1,47 +1,47 @@
-// for (var i = 1; i < 20; i++) {
-// 	console.lof(i)
-// }
-// for (let i = 0; i <= 200; i += 2) {
-// 	console.log(i);
-// }
-// for (let i = 1; i <= 20; i++) {
-// 	console.log("Love me, pet me! HSSSSSS!", i)
-// }
+for (var i = 1; i < 20; i++) {
+	console.lof(i)
+}
+for (let i = 0; i <= 200; i += 2) {
+	console.log(i);
+}
+for (let i = 1; i <= 20; i++) {
+	console.log("Love me, pet me! HSSSSSS!", i)
+}
 
-// for (let i = 0; i <= 20; i++) {
-// 	if (i % 2 === 0){
-// 		console.log(Math.random() * 100,"...human...why you taking pictures of me?...", i)
+for (let i = 0; i <= 20; i++) {
+	if (i % 2 === 0){
+		console.log(Math.random() * 100,"...human...why you taking pictures of me?...", i)
 		
 
-// 	}
-// 
-// for (let i = 1; i <= 100; i++) {
-// 	if (i % 3 === 0 && i % 5 === 0){
-// 		console.log("FizzBuzz");
-// 	}else if (i % 5 === 0){
-// 		console.log("Buzz");
-// 	}else if (i % 3 === 0){
-// 		console.log("Fizz");
-// 	}else {
-// 		console.log(i);
-// 	}
-// }
-// const thom = ["Thom", 1000, "Christchurch"]
-// const karolin = ["Karolin", 16, "New York"]
-// const kristyn = ["Kristyn", 5, "Pittsburgh"]
-// const matt = ["Matt H", 186, "Philadelphia"]
-// thom[0] = "Gameboy"
-// console.log(thom)
-// karolin[1] = 16++
-// console.log(karolin)
-// matt[3] = "Gotham City"
-// console.log(matt)
-// kristyn[3] = "Brooklyn"
-// console.log(kristyn)
-// const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
-// 	for (let i = 0; i < ninjaTurtles.length; i++){
-// 		console.log(ninjaTurtles[i].toUpperCase());
-// 	}
+	}
+
+for (let i = 1; i <= 100; i++) {
+	if (i % 3 === 0 && i % 5 === 0){
+		console.log("FizzBuzz");
+	}else if (i % 5 === 0){
+		console.log("Buzz");
+	}else if (i % 3 === 0){
+		console.log("Fizz");
+	}else {
+		console.log(i);
+	}
+}
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+thom[0] = "Gameboy"
+console.log(thom)
+karolin[1] = 16++
+console.log(karolin)
+matt[3] = "Gotham City"
+console.log(matt)
+kristyn[3] = "Brooklyn"
+console.log(kristyn)
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+	for (let i = 0; i < ninjaTurtles.length; i++){
+		console.log(ninjaTurtles[i].toUpperCase());
+	}
 
 const kristynsCloset = [
   "left shoe",
