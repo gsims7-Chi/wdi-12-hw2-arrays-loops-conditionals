55bf7713de809ea178f3118437107417ef2af518
Admin <GA-Admin@Admins-MacBook-Pro-2.local>
2018-06-13 19:11:48 -0500

    fluff this

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 // for (var i = 1; i < 20; i++) {
 // 	console.lof(i)
 // }
-for (let i = 0; i <= 200; i += 2) {
-	console.log(i);
+// for (let i = 0; i <= 200; i += 2) {
+// 	console.log(i);
+// }
+for (let i = 1; i <= 20; i++) {
+	console.log("Love me, pet me! HSSSSSS!", i)
+}
+
+for (let i = 0; i <= 20; i++) {
+	if (i % 2 === 0){
+		console.log(Math.random() * 100,"...human...why you taking pictures of me?...", i)
+		
+
+	}
 }
